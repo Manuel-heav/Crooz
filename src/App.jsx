22e7f59630705a86e8fba34033b38836efa5a0d9
manuel-heav <manuelheav32@gmail.com>
2023-12-04 14:59:35 +0300

    Did routes

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import AuthLayout from './_auth/AuthLayout'
+import SignIn from "./_auth/forms/SignIn"
+import Signup from "./_auth/forms/Signup"
+import Home from './_root/pages/Home'
+import RootLayout from './_root/RootLayout'
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route element={<SignIn />} path="/sign-in"/>
+        <Route element={<Signup />} path="/sign-up"/>
+      </Route>
+
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />}/>
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
