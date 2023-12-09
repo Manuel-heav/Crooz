5df7bbe8e230a807352c97cdbf7636ed859cc843
manuel-heav <manuelheav32@gmail.com>
2023-12-09 16:21:27 +0300

    color update

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -3,7 +3,7 @@ import { quotationMark } from '../assets'
 
 const TestimonialCard = ({testimonial}) => {
   return (
-    <div className='bg-[#dcb4a0] p-8 rounded-3xl shadow-xl my-8 mx-2'>
+    <div className='bg-[#e9c6b5] p-8 rounded-3xl shadow-xl my-8 mx-2'>
       <div className='flex justify-between'>
         <div className='flex gap-4'>
                 <img src={testimonial.profileImg} className='h-10 w-10 rounded-full'/>
