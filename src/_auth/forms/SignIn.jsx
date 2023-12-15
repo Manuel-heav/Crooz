171007db05f0ed706e32b4acc87d3d7e41a2f0a6
manuel-heav <manuelheav32@gmail.com>
2023-12-15 20:07:42 +0300

    got firebase working

--- src/_auth/forms/SignIn.jsx
+++ src/_auth/forms/SignIn.jsx
@@ -1,74 +1,128 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    getDownloadURL,
+    ref as storageRef,
+    uploadBytes,
+  } from "firebase/storage";
+import { collection, addDoc } from 'firebase/firestore';
+import { storage, db } from '../../util/Firebase';
 
 const SignIn = () => {
+
+    const [studentsName, setStudentsName] = useState("")
+    const [studentsLastName, setStudentsLastName] = useState("")
+    const [address, setAddress] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [grade, setGrade] = useState(0)
+    const [email, setEmail] = useState("")
+    const [schoolAddress, setSchoolAddress] = useState("")
+    const [studentPhoto, setStudentPhoto] = useState(null)
+    const [studentId, setStudentId] = useState(null)
+    const [parentId, setParentId] = useState(null)
+    const [progress, setProgress] = useState(0)
+
+
+    const handleUpload = () => {
+        const imageInputs = [studentPhoto, studentId, parentId];
+      
+        const uploadPromises = imageInputs.map((image) => {
+          if (image) {
+            const storageReference = storageRef(storage, `images/${image.name}`);
+            return uploadBytes(storageReference, image).then((snapshot) => {
+              return getDownloadURL(snapshot.ref);
+            });
+          }
+          return null;
+        });
+      
+        Promise.all(uploadPromises)
+          .then((urls) => {
+            // All uploads are complete, proceed with database update
+            const [studentPhotoUrl, studentIdUrl, parentIdUrl] = urls;
+      
+            addDoc(collection(db, 'parentsForm'), {
+                studentsName: studentsName,
+                studentsLastName: studentsLastName,
+                address: address,
+                phoneNumber: phoneNumber,
+                grade: grade,
+                email: email,
+                schoolAddress: schoolAddress,
+                studentPhotoUrl: studentPhotoUrl,
+                studentIdUrl: studentIdUrl,
+                parentIdUrl: parentIdUrl,
+              });
+            })
+          .catch((error) => {
+            console.error("Error uploading files:", error);
+            alert("Error uploading files. Please try again.");
+          });
+      };
   return (
     <div className='lg:pt-[20px] px-10 lg:px-80'>
      <h1 className='text-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-orange-400 from-orange-600'>Parent Form Registration</h1>
      <p className='text-center text-sm mb-10 mt-2'>(We will get back to you shortly)</p> 
 <form>
-    <div class="grid gap-6 mb-6 md:grid-cols-2 ">
+    <div className="grid gap-6 mb-6 md:grid-cols-2 ">
         <div>
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:">Student's First Name (የተማሪው የመጀመሪያ ስም)</label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
+            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:">Student's First Name (የተማሪው የመጀመሪያ ስም)</label>
+            <input onChange={e=>setStudentsName(e.target.value)}  type="text" id="first_name" className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
         </div>
         <div>
-            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:">Student's Last Name (የተማሪው የመጨረሻ ስም)</label>
-            <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required/>
+            <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:">Student's Last Name (የተማሪው የመጨረሻ ስም)</label>
+            <input onChange={e=>setStudentsLastName(e.target.value)} type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required/>
         </div>
         <div>
-            <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:">Address (አድራሻ)</label>
-            <input type="text" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Gullele" required/>
+            <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:">Address (አድራሻ)</label>
+            <input onChange={e=>setAddress(e.target.value)} type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Gullele" required/>
         </div>  
         <div>
-            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:">Parent's Phone Number (የቤተሰብ ስልክ)</label>
-            <input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678"  required/>
+            <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:">Parent's Phone Number (የቤተሰብ ስልክ)</label>
+            <input onChange={e=>setPhoneNumber(e.target.value)} type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678"  required/>
         </div>
         <div>
-            <label for="visitors" class="block mb-2 text-sm font-medium text-gray-900 dark:">Grade (የክፍል ደረጃ)</label>
-            <input type="number" id="visitors" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required/>
+            <label htmlFor="visitors" className="block mb-2 text-sm font-medium text-gray-900 dark:">Grade (የክፍል ደረጃ)</label>
+            <input onChange={e=>setGrade(e.target.value)} type="number" id="visitors" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required/>
         </div>
     </div>
-    <div class="mb-6">
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:">Parents' Email Address (ኢሜይል) (Optional)</label>
-        <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" />
+    <div className="mb-6">
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:">Parents' Email Address (ኢሜይል) (Optional)</label>
+        <input onChange={e=>setEmail(e.target.value)} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" />
     </div> 
-    <div class="mb-6">
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:">School Address (የትምህርት ቤት አድራሻ)</label>
-        <input type="text" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bole" required/>
+    <div className="mb-6">
+        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:">School Address (የትምህርት ቤት አድራሻ)</label>
+        <input onChange={e=>setSchoolAddress(e.target.value)} type="text" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bole" required/>
     </div> 
     
-    <div class="mb-6">
+    <div className="mb-6">
       
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:">Passport Sized Photo(student) (የተማሪ ፎቶ)</label>
-<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 p-2.5" aria-describedby="file_input_help" id="file_input" type="file"required/>
+    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:">Passport Sized Photo(student) (የተማሪ ፎቶ)</label>
+<input onChange={(e) => {setStudentPhoto(e.target.files[0])}} className="block w-full text-sm text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 p-2.5" aria-describedby="file_input_help" id="file_input" type="file"required/>
 
     </div>
 
-    <div class="mb-6">
+    <div className="mb-6">
       
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:">Student ID photo (የተማሪ መታወቂያ ፎቶ)</label>
-<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 p-2.5" aria-describedby="file_input_help" id="file_input" type="file"required/>
+    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:">Student ID photo (የተማሪ መታወቂያ ፎቶ)</label>
+<input onChange={(e) => {setStudentId(e.target.files[0])}}  className="block w-full text-sm text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 p-2.5" aria-describedby="file_input_help" id="file_input" type="file"required/>
 
     </div>
 
-    <div class="mb-6">
+    <div className="mb-6">
       
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:">Parent Government ID (የቤተሰብ መታወቂያ ፎቶ)</label>
-<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 p-2.5" aria-describedby="file_input_help" id="file_input" type="file"required/>
+    <label onChange={(e) => {setParentId(e.target.files[0])}} htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:">Parent Government ID (የቤተሰብ መታወቂያ ፎቶ)</label>
+<input className="block w-full text-sm text-gray-900 border border-gray-300 rounded cursor-pointer bg-gray-50 p-2.5" aria-describedby="file_input_help" id="file_input" type="file"required/>
 
     </div>
-    <div class="flex items-start mb-6">
-        <div class="flex items-center h-5">
-        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300  dark:focus:ring-blue-600 dark:ring-offset-gray-800" required/>
+    <div className="flex items-start mb-6">
+        <div className="flex items-center h-5">
+        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300  dark:focus:ring-blue-600 dark:ring-offset-gray-800" required/>
         </div>
-        <label for="remember" class="ms-2 text-sm font-medium text-gray-900">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
+        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
     </div>
 
-    <button className='px-8 py-3 rounded-md bg-[#DB7C26] text-white font-bold bg-gradient-to-br from-orange-600 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-orange-800 mb-5'>ተመዝገቡ</button>
-
-
+    <button onClick={handleUpload} className='px-8 py-3 rounded-md bg-[#DB7C26] text-white font-bold bg-gradient-to-br from-orange-600 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-orange-800 mb-5' type='button'>ተመዝገቡ</button>
 </form>
-
     </div>
   )
 }
--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -3,13 +3,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1TKxWuwxjv2CVUqAU-FI7ILfAKc6cp2g",
-  authDomain: "crooz-83fcd.firebaseapp.com",
-  projectId: "crooz-83fcd",
-  storageBucket: "crooz-83fcd.appspot.com",
-  messagingSenderId: "790051288631",
-  appId: "1:790051288631:web:95b3c1eefabeb78a2231b2",
-  measurementId: "G-MLZNQHKK6S",
+  apiKey: "AIzaSyCd7gGrZFC3pixJ_b7yoa1XuMOsWYQNOPA",
+  authDomain: "crooz-a58b5.firebaseapp.com",
+  projectId: "crooz-a58b5",
+  storageBucket: "crooz-a58b5.appspot.com",
+  messagingSenderId: "1053766965216",
+  appId: "1:1053766965216:web:ceaa8ca5f9ffdea6ea3d85",
+  measurementId: "G-PPRNQ4XSJQ",
 };
 
 const app = initializeApp(firebaseConfig);
