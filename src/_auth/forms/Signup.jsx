f8ad20ebc4c8496b91bd695a28949a175fdbcbaf
manuel-heav <manuelheav32@gmail.com>
2023-12-26 15:33:47 +0300

    updated liscense validation

--- src/_auth/forms/Signup.jsx
+++ src/_auth/forms/Signup.jsx
@@ -128,7 +128,7 @@ const Signup = () => {
                         </div> 
                         <div className="mb-6">
                             <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:">Liscence Plate Number (ታርጋ ቁጥር)</label>
-                            <input onChange={e=>setLiscence(e.target.value)} type="text" id="confirm_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="A12345" required/>
+                            <input onChange={e=>setLiscence(e.target.value)} type="text" id="confirm_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark: dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="A12345" maxLength={6} required/>
                         </div> 
                         <div className="mb-6">
                         
