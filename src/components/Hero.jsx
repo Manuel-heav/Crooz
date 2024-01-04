1acd09eabdaaf74059f15efd4a469a9dc48631bc
manuel-heav <manuelheav32@gmail.com>
2024-01-04 18:46:59 +0300

    minor tweaks again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,7 @@ const Hero = () => {
     <div className={`bg-[#ede7e4] py-40 w-full min-h-screen md:h-screen p-2 relative overflow-hidden block z-10 bg-cover bg-no-repeat bg-center before:content-[""] before:absolute before:inset-0 before:block before:bg-gradient-to-r before:from-[#4f311f] before:to-[#4f311f] before:opacity-[0.65] before:z-[-5]`} style={{ background: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
 
       <div className='h-[75vh] md:max-w-[1480px] max-w-[600px] m-auto w-full px-4 flex flex-col md:flex-row justify-between items-center md:px-10'>
-      <div 
-       className='flex-2'
-       >
-        <div className='flex'>
-            <img src={background} className='w-[100%] h-[100%] relative z-[5] mt-10 md:mt-0' />
-        </div>
-      </div>
+      
         <div className='flex-1'>
           <h2 className="bg-clip-text bg-gradient-to-r text-2xl md:text-5xl md:mt-0 mt-10 font-extrabold text-[#fff] nokia"> አስተማማኝ ጉዞዎች፣ ጭንቀት አልባ ወላጆች</h2>
           <h1 class="mb-4 text-2xl font-extrabold text-[#fff] dark:text-white md:text-6xl lg:text-7xl">የተሻለ እና ምቹ ትራንስፖርት</h1>
@@ -28,7 +22,13 @@ const Hero = () => {
           </a>
       </div>
 
-      
+      <div 
+       className='flex-2'
+       >
+        <div className='flex'>
+            <img src={background} className='w-[100%] h-[100%] relative z-[5] mt-10 md:mt-0' />
+        </div>
+      </div>
     </div>
         {/* <img src={ground}  className='opacity-[.0]'/> */}
     </div>
